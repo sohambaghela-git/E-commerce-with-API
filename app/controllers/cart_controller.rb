633aa835f2f4f50e11aa2ea8ac8f5b3e39dd9202
soham baghela <so.baghela@bestpeers.com>
2023-02-02 19:54:55 +0530

    cart_items_controller crud is completed

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,10 +1,3 @@
 class CartController < ApplicationController
-  skip_before_action :check_user
-
-  def index
-    cart = @current_user.cart
-    cart_items = CartItem.where(cart_id: cart.id)
-    render json: cart_items
-  end
 
 end
