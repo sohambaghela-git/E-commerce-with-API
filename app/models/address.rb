760f48a0556c43eae0307726fe1c01ca3a936c7b
soham baghela <so.baghela@bestpeers.com>
2023-02-10 14:05:04 +0530

    updated validation for all model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user
   has_many :orders
+  
   validates :colony, :pincode, :state, :city, presence: true
+  validates :pincode, numericality: { only_integer: true }
+
 end
